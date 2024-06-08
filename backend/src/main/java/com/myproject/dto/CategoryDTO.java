41156d77f95cac6b24326170611cc0334fb32ffc
claraint <3036383387@qq.com>
2024-06-08 17:02:07 +0800

    可以上传和查询分类了

---
+++ backend/src/main/java/com/myproject/dto/CategoryDTO.java
@@ -0,0 +1,32 @@
+package com.myproject.dto;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String parentName;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+}
